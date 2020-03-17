be0fe7f0c405c0e0a65d762cee30c057e21b2dfb
Noah Wilder <noahleviwilder@gmail.com>
2020-03-16 20:22:36 -0400

    Added documentation for `handler` parameter of `ignoreFailure(_:)` operator.

--- Sources/CustomCombine/Operators/IgnoreFailure.swift
+++ Sources/CustomCombine/Operators/IgnoreFailure.swift
@@ -21,7 +21,8 @@ public extension Publisher {
     /// Ignores all upstream errors, but passes along upstream elements.
     ///
     /// The failure type of this publisher is `Never`.
-    /// - Returns: A publisher that ignores all upstream errors.
+    /// - Parameter handler: A closure that handles upstream errors.
+    /// - Returns: A publisher that handles and ignores all upstream errors.
     func ignoreFailure(_ handler: @escaping (_ error: Failure) -> Void) -> Publishers.IgnoreFailure<Self> {
         return Publishers.IgnoreFailure(upstream: self, handler: handler)
     }
