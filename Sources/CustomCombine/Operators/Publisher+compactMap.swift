f0c53e959ddfd147900075710343d63ae7eb454d
Noah Wilder <noahleviwilder@gmail.com>
2020-03-20 21:31:04 -0400

    Added `compactMap` operator that takes a `KeyPath` .

---
+++ Sources/CustomCombine/Operators/Publisher+compactMap.swift
@@ -0,0 +1,19 @@
+//
+//  Publisher+compactMap.swift
+//  
+//
+//  Created by Noah Wilder on 2020-03-20.
+//
+
+import Combine
+
+public extension Publisher {
+    /// Publishes the value of any optional key path that has a value.
+    /// 
+    /// - Parameter keyPath: The key path of a property on `Output`.
+    /// - Returns: A publisher that republishes all non-`nil` values of the key path.
+    func compactMap<T>(_ keyPath: KeyPath<Output, T?>) -> Publishers.CompactMap<Self, T> {
+        return self
+            .compactMap { $0[keyPath: keyPath] }
+    }
+}
