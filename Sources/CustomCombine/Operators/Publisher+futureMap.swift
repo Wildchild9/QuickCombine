f176e9c8d11fc50af3ff08eaf3d1d0c3f0149722
Noah Wilder <noahleviwilder@gmail.com>
2020-03-20 21:34:58 -0400

    Added `FutureMap` publisher and corresponding operator.

---
+++ Sources/CustomCombine/Operators/Publisher+futureMap.swift
@@ -0,0 +1,23 @@
+//
+//  Publisher+futureMap.swift
+//  
+//
+//  Created by Noah Wilder on 2020-03-20.
+//
+
+import Combine
+
+public extension Publisher {
+    /// Transforms all elements from the upstream publisher asyncronously with a provided closure.
+    ///
+    /// - Important: The provided `transform` closure may only invoke its `promise` closure once. Any additional invocations of `promise` will be ignored.
+    ///
+    /// - Parameters:
+    ///   - transform: A closure that takes an element and a promise as its parameters and, using the promise, asyncronously produces a new element.
+    ///   - value: The upstream element.
+    ///   - promise: The closure to invoke in the future, when an element is available.
+    /// - Returns: A publisher that uses a promise in the provided closure to map elements from the upstream publisher to new elements that it then publishes.
+    func futureMap<T>(transform: @escaping (_ value: Output, _ promise: Publishers.FutureMap<Self, T>.Promise) -> Void) -> Publishers.FutureMap<Self, T> {
+        return Publishers.FutureMap(upstream: self, transform: transform)
+    }
+}
---
+++ Sources/CustomCombine/Publishers/FutureMap.swift
@@ -0,0 +1,44 @@
+//
+//  FutureMap.swift
+//  
+//
+//  Created by Noah Wilder on 2020-03-20.
+//
+
+import Combine
+
+public extension Publishers {
+    
+    /// A publisher that eventually transforms each element from the upstream publisher into a single value with a provided promise closure.
+    ///
+    /// - Important: Each upstream element may only invoke a promise closure once. Any subsequent invocations of the promise closure will be ignored.
+    struct FutureMap<Upstream, Output>: Publisher where Upstream: Publisher {
+        public typealias Failure = Upstream.Failure
+        
+        /// A type that represents a closure to invoke in the future when an element is available.
+        ///
+        /// The promise closure receives one paramater: a single element published by a `FutureMap` publisher.
+        public typealias Promise = (Output) -> Void
+        
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+        
+        /// The closure that asyncronously transforms all upstream elements using a promise closure that can be invoked only once. Any subsequent invocations of the promise closure will be ignored.
+        public let transform: (Upstream.Output, Promise) -> Void
+        
+        public init(upstream: Upstream, transform: @escaping (_ value: Upstream.Output, _ promise: Promise) -> Void) {
+            self.upstream = upstream
+            self.transform = transform
+        }
+        
+        public func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
+            upstream
+                .flatMap { value in
+                    Future<Output, Failure> { promise in
+                        self.transform(value) { promise(.success($0)) }
+                    }
+                }
+                .subscribe(subscriber)
+        }
+    }
+}
