c0aeebfb5c35eb58864ae4a90b56fcf6b6817d89
Noah Wilder <noahleviwilder@gmail.com>
2020-03-23 23:22:51 -0400

    Created `Then` publisher.

--- Sources/QuickCombine/Publishers/Then.swift
+++ Sources/QuickCombine/Publishers/Then.swift
@@ -6,3 +6,32 @@
 //
 
 import Combine
+
+public extension Publishers {
+    /// A publisher that ignores all upstream elements and uses a new publisher to emit elements downstream.
+    struct Then<Upstream, NextPublisher>: Publisher where Upstream: Publisher, NextPublisher: Publisher, NextPublisher.Failure == Upstream.Failure {
+        
+        public typealias Failure = Upstream.Failure
+        public typealias Output = NextPublisher.Output
+        
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+        
+        /// The closure from which a new publisher will be created that emits elements downstream upon receiving a successful completion for the upstream publisher.
+        public let transform: () -> NextPublisher
+        
+        public init(upstream: Upstream, transform: @escaping () -> NextPublisher) {
+            self.upstream = upstream
+            self.nextPublisher = nextPublisher
+        }
+    
+        public func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+            upstream
+                .ignoreOutput()
+                .flatMap { _ in
+                    nextPublisher
+                }
+                .subscribe(subscriber)
+        }
+    }
+}
